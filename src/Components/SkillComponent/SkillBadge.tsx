689c91bda236004358d3b6aa851540d194aca526
Alex Flok <alexflok2018@gmail.com>
2022-10-16 01:10:53 -0400

    added back button as resuable component; working on skill badge component

---
+++ src/Components/SkillComponent/SkillBadge.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+type TSkillBadge = {
+  skillName: string;
+  type: string;
+  description?: string;
+};
+
+const SkillBadgeComponent: FC<TSkillBadge> = ({ skillName, type }) => {
+  return <></>;
+};
+
+export default SkillBadgeComponent;
